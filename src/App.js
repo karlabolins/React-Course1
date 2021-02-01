fa6c421c4dbcd8f9a240fddbac8333ffa82b1d30
karlabolins <78203762+karlabolins@users.noreply.github.com>
2021-02-01 22:40:50 +0800

    Lecture 53 Fix
    Fixed some wrong code. Version is now up to date with lecture 53.

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button style = {style} onClick={() => this.togglePersonsHandler}>Switch Name</button>
+        <button style = {style} onClick={this.togglePersonsHandler}>Switch Name</button>
           {this.state.showPersons ?
           <div>
             <Person
